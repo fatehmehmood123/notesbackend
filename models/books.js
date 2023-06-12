3186c27d61b592b1cc6ecbb77eac0ed30bdb41f6
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-06-12 17:18:57 +0500

    Automated commit message

--- models/books.js
+++ models/books.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dateObj = new Date();
+dateObj.setHours(dateObj.getHours() + 5);
 const formattedDate = dateObj.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
 
 const bookSchema = new mongoose.Schema({
