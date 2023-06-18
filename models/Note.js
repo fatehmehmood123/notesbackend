3444f08244695af5d06bd6c3b7531c981be15173
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-06-18 14:56:01 +0500

    Automated commit message

--- models/Note.js
+++ models/Note.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const dateObj = new Date();
-// dateObj.setHours(dateObj.getHours() + 5);
-const formattedDate = dateObj.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
+const options = { 
+  year: 'numeric', 
+  month: 'long', 
+  day: 'numeric', 
+  hour: 'numeric', 
+  minute: 'numeric', 
+  hour12: true,
+  timeZone: 'Asia/Karachi'
+};
+const formattedDate = dateObj.toLocaleDateString("en-US", options);
 
 const noteSchema = new mongoose.Schema({
   userId: {
